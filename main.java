ba504b363cf69fbd21cfb53304782ce9e8ddfdac
Samrat <108344579+Samysr17@users.noreply.github.com>
2023-05-03 18:45:23 +0530

    Inheritance ,Runtime Polymorphism

--- main.java
+++ main.java
@@ -1,11 +1,10 @@
-package oops.Vector.Inheritance;
+package oops.Inh_2.prac_3;
 
 public class main {
     public static void main(String[] args) {
-        fig a[]=new fig[2];
-        a[0]= new rectangle(1,2);
-        a[1]=new tri(1,2);
-        a[0].area();
-        a[1].area();
+        student a[]=new student[5];
+        a[0]=new off("Samrat","Male","CSE",9,"Microsoft",2000000,001);
+        a[1]=new ent("Sahil","Male","CSE",10,"Amazon","IT",2000,100000000);
+        a[2]=new high("Aman","Female","CSE",11,"MBA","RCOEM",11,100);
     }
 }
---
+++ student.java
@@ -0,0 +1,14 @@
+package oops.Inh_2.prac_3;
+
+public class student {
+    String Name;
+    String Gender;
+    String Deptt;
+    double GPA;
+    student(String Name,String Gender,String Deptt,double GPA){
+        this.Name=Name;
+        this.Gender=Gender;
+        this.Deptt=Deptt;
+        this.GPA=GPA;
+    }
+}
