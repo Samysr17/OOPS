c0de13cee51d36ab679a14849cedcbe37d6bbfbd
Samrat <108344579+Samysr17@users.noreply.github.com>
2023-06-06 22:29:40 +0530

    exception handling in JAVA
    User Defined

---
+++ exception.java
@@ -0,0 +1,9 @@
+package prac_5;
+
+public class exception extends Exception{
+    String s;
+    exception(String s){
+       System.out.println(s);
+    }
+    
+}
--- main.java
+++ main.java
@@ -1,17 +1,14 @@
-package prac_4;
-public class main{
-public static void main(String[] args){
-    Save_acc s1=new Save_acc(11,"Samrat",50000);
-    s1.update_interest();
-    s1.withdraw(1000);
-    FD s2=new FD(12,"Samrat",3,1000,1);
-    s2.update_interest();
-    s2.close_account();
-    Save_acc s3=new Save_acc(11,"Samrat",50000);
-    s3.update_interest();
-    s3.withdraw(1000);
-    FD s4=new FD(12,"Samrat",3,1000,1);
-    s4.update_interest();
-    s4.close_account();
-}
+package prac_5;
+
+public class main {
+    public static void main(String[] args) {
+        try{
+            Student s1=new Student();
+            s1.check("Samrat", 10, "BE20CSU110", "b@1236");
+        }
+        catch(exception e){
+            
+        }
+        
+    }
 }
